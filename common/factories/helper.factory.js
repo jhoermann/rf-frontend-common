5afda5177ea8920cf80caeae6bf2d6e586924640
Felix <ff@rapidfacture.com>
2018-02-19 22:06:08 +0100

    improve docs of helper factory watchers

--- common/factories/helper.factory.js
+++ common/factories/helper.factory.js
@@ -1,7 +1,7 @@
 /**
  * @module helperFactory
  * @desc common functions
- * @version 0.1.0
+ * @version 0.1.1
  */
 
 app.factory('helperFactory', ['$state', '$rootScope', function ($state, $rootScope) {
@@ -38,60 +38,69 @@ app.factory('helperFactory', ['$state', '$rootScope', function ($state, $rootSco
    }
 
    /**
-    * @example var watch = new helperFactory.watcher($scope, onChangeFunction );
+    * @example
+    *
+    * var watch = new helperFactory.watcher($scope, onChangeFunction );
+    * watch.addWatcher('customer');
+    * watch.addWatcher('order')
+    *
+    * watch.startAll();
+    * watch.stopAll();
+    *
     */
    function _watch (scope, defaultChangeFunction) {
-      var self = this;
 
+      var self = this;
+      self.list = {};
       self.scope = scope;
+
       self.watchChange = defaultChangeFunction || function (newVal, oldVal) {
          if (newVal !== oldVal) scope.unsaved = true; // tell user, he has to save
       };
 
-      self.list = {};
-
-      self.unbind = function (name) {
-         self.stop(name);
-         delete self.list[name];
-      };
-
-      self.unbindAll = function () {
-         for (var watchName in self.list) {
-            self.unbind(watchName);
-         }
-      };
-
-      self.start = function (name) {
-         if (self.list[name] && self.list[name].process === false) {
-            self.list[name].process = scope.$watch(name, self.list[name].changeFunction, true);
-         }
-      };
-
-      self.startAll = function () {
-         for (var watchName in self.list) {
-            self.start(watchName);
-         }
-      };
-
-      self.stop = function (name) {
-         if (self.list[name] && self.list[name].process !== false) {
-            self.list[name].process();
-            self.list[name].process = false;
-         }
-      };
-
-      self.stopAll = function () {
-         for (var watchName in self.list) {
-            self.stop(watchName);
-         }
-      };
-
+      // create a watcher in list
       self.addWatcher = function (scopeObjectToWatch, changeFunction) {
          changeFunction = changeFunction || self.watchChange;
          self.list[scopeObjectToWatch] = {
             process: scope.$watch(scopeObjectToWatch, changeFunction, true),
             changeFunction: changeFunction
          };
+      };
+
+      // main functions: start and stop all watchers
+      self.startAll = function () {
+         for (var watchName in self.list) {
+            self.start(watchName);
+         }
+      };
+      self.stopAll = function () {
+         for (var watchName in self.list) {
+            self.stop(watchName);
+         }
+      };
+
+      // delete a watchers
+      self.unbind = function (name) {
+         self.stop(name);
+         delete self.list[name];
+      };
+      self.unbindAll = function () {
+         for (var watchName in self.list) {
+            self.unbind(watchName);
+         }
+      };
+
+      // start/stop a single watcher
+      self.start = function (name) {
+         if (self.list[name] && self.list[name].process === false) {
+            self.list[name].process = scope.$watch(name, self.list[name].changeFunction, true);
+         }
+      };
+      self.stop = function (name) {
+         if (self.list[name] && self.list[name].process !== false) {
+            self.list[name].process();
+            self.list[name].process = false;
+         }
       };
    }
 
