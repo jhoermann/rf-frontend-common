a784b1f75824b79824110ac4f47ce1fbf26ca7f8
Felix <ff@rapidfacture.com>
2018-02-26 20:29:27 +0100

    refactor confirmInput directive

--- common/modal/confirmInput/directive.js
+++ common/modal/confirmInput/directive.js
@@ -5,11 +5,7 @@ app.directive('rfModalConfirm', [function () {
       scope: { modal: '=', lang: '=' },
       link: function ($scope, elem, attr, ctrl) {
          $scope.modal.onSuccess = $scope.modal.onSuccess || function () {};
-         $scope.modal.onFailure = $scope.modal.onFailure || function () {};
-
-         $scope.isDisabled = true
-         $scope.param = 'param' // this should get whatever parameter needs to be checken, e.g. name
-         $scope.paramInput = ''
+         $scope.confirmText = $scope.modal.confirmText || 'CONFIRM';
       }
    };
 }]);
