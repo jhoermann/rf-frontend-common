29e8b21ca7770da7f690c17b84aface95ef3b34a
Uli Köhler <ulikoehler@online.de>
2018-06-28 03:43:01 +0200

    Added drag&drop file upload directive

---
+++ common/dragndropUpload/directive.js
@@ -0,0 +1,30 @@
+/**
+ * Drag & drop upload wrapper. Use like this:
+ * <div class="uploadSection" dragndrop-upload onupload="onupload">
+ *      <!-- content: whatever you need, has no effect on upload -->
+ * </div>
+ *
+ * where $scope.onupload is a function(content, filename, mimetype, size).
+ */
+app.directive('dragndropUpload', ['$parse', function ($parse) {
+   return {
+      restrict: 'A', // called on class "uploadDirective"
+      link: function ($scope, elem, attr) {
+         $scope.onupload = $parse(attr.onupload);
+
+         // Initialize handling of drag & drop events
+         initializeDragAndDrop(angular.element(elem)[0], function (files) {
+            // Iterate over files
+            for (var i = 0; i < files.length; i++) {
+               readFileIntoMemory(files[i], function (fileInfo) {
+                  // Call callback
+                  $scope.onupload(
+                     fileInfo.content, fileInfo.name, fileInfo.type,
+                     fileInfo.size);
+               });
+            }
+         });
+
+      }
+   };
+}]);
